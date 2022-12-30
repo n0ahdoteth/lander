372d6aa8ce882c1d4e4595cd3c5c02fc1351b3b5
Subtain Farooq <subtainfrq@gmail.com>
2022-12-30 11:18:49 -0800

    Space decressed between testimonial and CTA

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -24,7 +24,7 @@ function Testimonials() {
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20">
+        <div className="pt-12 md:pt-20">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
